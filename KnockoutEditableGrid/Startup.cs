2d016cc1a5cdf14cb6fc142d630c6a60d529172b
randelramirez <randelramirez1@gmail.com>
2016-03-25 02:24:36 +0800

    Initial commit

---
+++ KnockoutEditableGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnockoutEditableGrid.Startup))]
+namespace KnockoutEditableGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
